b7d84ef6e7644d1dfd411909d6083258ee84b487
Amogh Prabhu <amoghprabhu@hotmail.com>
2022-08-26 19:24:38 +0530

    changes

--- hackelth_app/lib/pages/chat_page.dart
+++ hackelth_app/lib/pages/chat_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:hackelth_app/model/message_model.dart';
 import 'package:hackelth_app/service/api.dart';
 import 'package:hackelth_app/utils/constants.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 String randomString() {
@@ -67,6 +68,57 @@ class _ChatPageState extends State<ChatPage> {
     getReply(message.text);
   }
 
+  void _handleImageSelection() async {
+    var option = ImageSource.gallery;
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select image source'),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                option = ImageSource.camera;
+                Navigator.pop(context);
+              },
+              child: const Text('Camera'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                option = ImageSource.gallery;
+                Navigator.pop(context);
+              },
+              child: const Text('Gallery'),
+            ),
+          ],
+        );
+      },
+    );
+    final result = await ImagePicker().pickImage(
+      imageQuality: 70,
+      maxWidth: 1440,
+      source: option,
+    );
+
+    if (result != null) {
+      final bytes = await result.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: randomString(),
+        name: result.name,
+        size: bytes.length,
+        uri: result.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,10 +135,27 @@ class _ChatPageState extends State<ChatPage> {
         ),
       ),
       body: Chat(
-        theme: const DefaultChatTheme(),
+        theme: const DefaultChatTheme(
+          attachmentButtonIcon: Icon(
+            Icons.add_a_photo,
+            color: Colors.white,
+          ),
+        ),
         messages: _messages,
         onSendPressed: _handleSendPressed,
         user: _user,
+        onAttachmentPressed: _handleImageSelection,
+        showUserAvatars: true,
+        avatarBuilder: (userId) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: SizedBox(
+              height: 40,
+              child: Image.network(
+                  "https://texapi.net/wp-content/uploads/2022/05/icon.png"),
+            ),
+          );
+        },
       ),
     );
   }
