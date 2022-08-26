c3395d7a0d011b5b403effbe16314013c3721b7c
AishwaryaJadhav <apjadhav_b20@it.vjti.ac.in>
2022-08-27 01:34:27 +0530

    adding firestore

--- hackelth_app/lib/main.dart
+++ hackelth_app/lib/main.dart
@@ -1,7 +1,11 @@
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hackelth_app/pages/home_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,12 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage() 
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage());
   }
 }
-
