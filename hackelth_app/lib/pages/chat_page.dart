620586b380a3cbfa0a04a63dc24f94a2e67340aa
Amogh Prabhu <amoghprabhu@hotmail.com>
2022-08-27 13:14:15 +0530

    changes

--- hackelth_app/lib/pages/chat_page.dart
+++ hackelth_app/lib/pages/chat_page.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, must_be_immutable, depend_on_referenced_packages, unused_field, prefer_final_fields, unused_element, avoid_print
 
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:hackelth_app/model/message_model.dart';
 import 'package:hackelth_app/service/api.dart';
+import 'package:hackelth_app/service/storage_service.dart';
 import 'package:hackelth_app/utils/constants.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
@@ -30,6 +33,8 @@ class _ChatPageState extends State<ChatPage> {
   final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
   final _botUser = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d2f32c');
   SolutionService service = SolutionService();
+  var _state = false;
+  Storare storare = Storare();
 
   @override
   void initState() {
@@ -39,6 +44,7 @@ class _ChatPageState extends State<ChatPage> {
 
   getReply(String text) {
     service.getMessages(text).then((value) {
+      _state = value.query == '1';
       setState(() {
         final text = types.TextMessage(
           author: _botUser,
@@ -51,13 +57,28 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
+  getPrediction(String url) {
+    service.getSkinDisease(url).then((value) {
+      setState(() {
+        final text = types.TextMessage(
+          author: _botUser,
+          createdAt: DateTime.now().millisecondsSinceEpoch,
+          id: randomString(),
+          text:
+              "You have symptoms of ${value.disease}.\nNote: Users should not completely rely on information provided. Please contant your physician or other healthcare provider.",
+        );
+        _addMessage(text);
+      });
+    });
+  }
+
   void _addMessage(types.Message message) {
     setState(() {
       _messages.insert(0, message);
     });
   }
 
-  void _handleSendPressed(types.PartialText message) {
+  void _handleSendPressed(types.PartialText message) async {
     final textMessage = types.TextMessage(
       author: _user,
       createdAt: DateTime.now().millisecondsSinceEpoch,
@@ -65,10 +86,25 @@ class _ChatPageState extends State<ChatPage> {
       text: message.text,
     );
     _addMessage(textMessage);
-    getReply(message.text);
+    var list = ['yes', 'yeah', 'sure', 'ok', 'alright', 'ha', 'fine'];
+    bool check = false;
+    for (String s in list) {
+      if (s == message.text.toLowerCase()) {
+        check = true;
+        break;
+      }
+    }
+    if (_state && check) {
+      String url = await _handleImageSelection();
+      print(url);
+      getPrediction(url);
+      _state = false;
+    } else {
+      getReply(message.text);
+    }
   }
 
-  void _handleImageSelection() async {
+  Future<String> _handleImageSelection() async {
     var option = ImageSource.gallery;
     await showDialog(
       context: context,
@@ -117,6 +153,14 @@ class _ChatPageState extends State<ChatPage> {
 
       _addMessage(message);
     }
+
+    String downloadURL = "";
+    final destination = "${DateTime.now().hashCode}";
+
+    await FirebaseStorage.instance.ref(destination).putFile(File(result!.path));
+    downloadURL =
+        await FirebaseStorage.instance.ref(destination).getDownloadURL();
+    return downloadURL;
   }
 
   @override
@@ -127,41 +171,42 @@ class _ChatPageState extends State<ChatPage> {
         return await true;
       },
       child: Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        foregroundColor: GEHackTheme.redColor,
-        elevation: 0.0,
-        centerTitle: true,
-        title: Text(
-          "Chatbot",
-          style: GEHackTheme.geStyle(
-              size: 30, weight: FontWeight.w600, color: Colors.black),
-        ),
-      ),
-      body: Chat(
-        theme: const DefaultChatTheme(
-          attachmentButtonIcon: Icon(
-            Icons.add_a_photo,
-            color: Colors.white,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          foregroundColor: GEHackTheme.redColor,
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text(
+            "Chatbot",
+            style: GEHackTheme.geStyle(
+                size: 30, weight: FontWeight.w600, color: Colors.black),
           ),
         ),
-        messages: _messages,
-        onSendPressed: _handleSendPressed,
-        user: _user,
-        onAttachmentPressed: _handleImageSelection,
-        showUserAvatars: true,
-        avatarBuilder: (userId) {
-          return Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: SizedBox(
-              height: 40,
-              child: Image.network(
-                  "https://texapi.net/wp-content/uploads/2022/05/icon.png"),
+        body: Chat(
+          theme: const DefaultChatTheme(
+            attachmentButtonIcon: Icon(
+              Icons.add_a_photo,
+              color: Colors.white,
             ),
-          );
-        },
-      ),),
+          ),
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
+          user: _user,
+          onAttachmentPressed: _handleImageSelection,
+          showUserAvatars: true,
+          avatarBuilder: (userId) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: SizedBox(
+                height: 40,
+                child: Image.network(
+                    "https://texapi.net/wp-content/uploads/2022/05/icon.png"),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
