e0c00981209ca3f389a58506a7a984320f617975
Sayali <sayalipanch17@gmail.com>
2022-08-27 17:15:26 +0530

    Added changed

--- hackelth_app/lib/main.dart
+++ hackelth_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:hackelth_app/pages/home_page.dart';
 import 'package:hackelth_app/pages/map_page.dart';
--- hackelth_app/lib/pages/map_page.dart
+++ hackelth_app/lib/pages/map_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -12,9 +14,66 @@ class CurrentLocationScreen extends StatefulWidget {
 class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
   late GoogleMapController googleMapController;
 
-  static const CameraPosition initialCameraPosition = CameraPosition(target: LatLng(19.0222,72.8561), zoom: 14);
+  static const CameraPosition initialCameraPosition =
+      CameraPosition(target: LatLng(19.0222, 72.8561), zoom: 7);
 
   Set<Marker> markers = {};
+
+  bool isComplete = true;
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      markers.add(
+        const Marker(
+          markerId: MarkerId('1'),
+          position: LatLng(18.535753, 73.877304),
+          infoWindow: InfoWindow(
+            title: 'Wipro GE Healthcare Pvt. Ltd.',
+          ),
+        ),
+      );
+
+      markers.add(
+        const Marker(
+          markerId: MarkerId('2'),
+          position: LatLng(18.617933, 73.760086),
+          infoWindow: InfoWindow(
+            title: 'Wipro GE Healthcare Pvt. Ltd.',
+          ),
+        ),
+      );
+
+      markers.add(
+        const Marker(
+          markerId: MarkerId('3'),
+          position: LatLng(23.237299, 77.434174),
+          infoWindow: InfoWindow(
+            title: 'Wipro GE Healthcare Pvt. Ltd.',
+          ),
+        ),
+      );
+
+      markers.add(
+        const Marker(
+          markerId: MarkerId('4'),
+          position: LatLng(12.994883, 77.758919),
+          infoWindow: InfoWindow(
+            title: 'Wipro GE Healthcare Pvt. Ltd.',
+          ),
+        ),
+      );
+
+      markers.add(
+        const Marker(
+          markerId: MarkerId('5'),
+          position: LatLng(12.985350, 77.752037),
+          infoWindow: InfoWindow(
+            title: 'Wipro GE Healthcare Pvt. Ltd.',
+          ),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,27 +87,38 @@ class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
         markers: markers,
         zoomControlsEnabled: false,
         mapType: MapType.normal,
-        onMapCreated: (GoogleMapController controller) {
-          googleMapController = controller;
+        onMapCreated: (c) {
+          googleMapController = c;
+          _onMapCreated(c);
+          setState(() {});
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: FloatingActionButton(
         onPressed: () async {
+          setState(() {
+            isComplete = false;
+          });
           Position position = await _determinePosition();
-
-          googleMapController
-              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(position.latitude, position.longitude), zoom: 14)));
-
-
-          markers.clear();
-
-          markers.add(Marker(markerId: const MarkerId('currentLocation'),position: LatLng(position.latitude, position.longitude)));
+          setState(() {
+            isComplete = true;
+          });
+          googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                  zoom: 11)));
+          markers.add(Marker(
+              markerId: const MarkerId('currentLocation'),
+              position: LatLng(position.latitude, position.longitude)));
 
           setState(() {});
-
         },
-        label: const Text("Current Location"),
-        icon: const Icon(Icons.location_history),
+        child: !isComplete
+            ? SizedBox(
+                height: 10,
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ))
+            : Icon(Icons.location_on),
       ),
     );
   }
@@ -60,7 +130,7 @@ class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
 
     if (!serviceEnabled) {
-      return Future.error('Location services are disabled');
+      return Future.error('Location services are disabled!');
     }
 
     permission = await Geolocator.checkPermission();
@@ -81,4 +151,222 @@ class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
 
     return position;
   }
+}
+
+class Utils {
+  static String mapStyle = '''
+  [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#263c3f"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6b9a76"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#38414e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#212a37"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9ca5b3"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#1f2835"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#f3d19c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2f3948"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#515c6d"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  }
+]
+''';
 }
